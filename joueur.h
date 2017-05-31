a499d283fe8fe5f2a83d153ea9038f085549635b
Cavallones <q.sofian@gmail.com>
2017-05-31 12:31:52 +0200

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -3,21 +3,30 @@
 
 using namespace std;
 
-class Joueur{
+struct player{
+	string name;
+	int productivite;
+	int power;
+	int score;
+	int ammo;
+	int pos[2];
+};
+
+class game{
 	private:
-		int life;
-		int power;
-		int pos[2];
-		bool isAlive;
-		vector<vector<string>> level;
+		int taille_level[2];
+		int nbPlayers;
+		int nbTurn;
+		vector<vector<string> > level;
+		player joueur;
 		
 
 	public:
-		Joueur();
-		Joueur(int pos[2]);
+		game();
+		game(int pos[2]);
 		void deplacement(string instruction);
-		bool validation_deplacement(stringh instruction);
-		string best_move(string trial);
+		bool validation_deplacement(string instruction);
+		void best_move(string trial);
 		string best_move_for_first();
 		string best_move_for_two();
 		string best_move_for_third();
